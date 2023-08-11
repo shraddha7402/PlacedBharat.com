51b2a800b38a0e13321b538bd121bdccfc6f0111
Shraddha Hajari <hajarishraddha519@gmail.com>
2023-08-11 17:37:01 +0530

    My final year project

---
+++ php/footer.php
@@ -0,0 +1,45 @@
+<footer id="footer" class="footer">
+
+
+
+    <div class="footer-legal text-center">
+        <div class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
+
+            <div class="d-flex flex-column align-items-center align-items-lg-start">
+                <div class="copyright">
+                    &copy; Copyright <strong><span>Code Stomp</span></strong>. All Rights Reserved
+                </div>
+                <div class="credits">
+
+                    <a href="https://engg.mit.asia/">MIT-PlacementCell</a>
+                </div>
+            </div>
+
+            <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
+                <a href="#" class="linkedin" onclick="location.href='https://www.linkedin.com/in/shraddha-hajari-623b14219/'"><i class="bi bi-linkedin"></i></a>
+                <a href="#" class="github"onclick="location.href='https://github.com/shraddha7402'"><i class="bi bi-github"></i></a>
+                <a href="#" class="instagram" onclick="location.href='https://www.instagram.com/_shraddha_7402/'"><i class="bi bi-instagram"></i></a>
+                <!--
+                <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>-->
+                
+            </div>
+
+        </div>
+    </div>
+
+</footer>
+
+<a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<div id="preloader"></div>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
